8f0d5345479985ca96bc345398b3a4e89ddcbeb2
Kviraj <kawa.135viraj@gmail.com>
2024-05-30 18:08:20 +0530

    bugfix: responsiveness.

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -12,8 +12,8 @@ const HeroSection = () => {
     "I specialize in delivering innovative and efficient solutions that enhance user experiences, streamline operations, and drive business success. My expertise lies in creating scalable applications that are both reliable and robust, ensuring seamless integration, optimal performance, and future-ready capabilities for diverse business needs.";
   const words = ["Full stack", "Software", "Frontend", "Backend"];
   return (
-    <div className="flex gap-2 flex-wrap md:items-center sm:items-center md:justify-center sm:justify-center">
-      <div className="flex flex-col justify-center items-center gap-5 ">
+    <div className="flex gap-2 flex-wrap justify-center">
+      <div className="items-center flex flex-col gap-5 ">
         <Image
           src={ProfilePhoto}
           alt="profile image"
@@ -21,15 +21,13 @@ const HeroSection = () => {
           width={300}
           className="border-blue-500 border rounded-full mt-5 h-60 w-60"
         />
-        <div>
+        <div className="flex">
           <FlipWords words={words} className="font-extrabold" />
-          developer
+          <p className="">developer. </p>
         </div>
       </div>
 
-      <div className=" items-center flex flex-col gap-2">
-        <TextGenerateEffect words={sent} />
-      </div>
+      <TextGenerateEffect words={sent} />
     </div>
   );
 };
