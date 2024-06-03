5f53d7580b446a96c26cbc7cbebe5cfe98f8fc0e
Kviraj <kawa.135viraj@gmail.com>
2024-06-03 12:49:07 +0530

    feat: technologies section.

--- app/components/Technologies.tsx
+++ app/components/Technologies.tsx
@@ -1,61 +1,74 @@
-"use client";
 import {
-  SiCloudflare,
+  SiCplusplus,
   SiCss3,
+  SiDaisyui,
+  SiDbeaver,
+  SiDocker,
+  SiExpress,
+  SiFastify,
   SiFigma,
   SiFirebase,
   SiGit,
+  SiGithub,
   SiHtml5,
   SiJavascript,
-  SiJest,
-  SiMarkdown,
+  SiMaterialdesign,
+  SiMongodb,
+  SiMongoose,
+  SiMoonrepo,
   SiMysql,
   SiNextdotjs,
   SiNodedotjs,
-  SiPostgresql,
-  SiPrisma,
-  SiPython,
+  SiPostman,
   SiReact,
+  SiRedux,
+  SiSequelize,
+  SiSocketdotio,
+  SiStripe,
   SiTailwindcss,
+  SiTrpc,
+  SiTurborepo,
   SiTypescript,
   SiVisualstudiocode,
-  SiVite,
 } from "@icons-pack/react-simple-icons";
-import { Marquee } from "@tszhong0411/ui";
-import { ZapIcon } from "lucide-react";
+import Heading from "./Heading";
 
 const Technologies = () => {
   return (
-    <div className="border-1 rounded-md container mt-10 shadow-blurred-border">
-      <div className="flex border-2 h-60 flex-col gap-2 overflow-hidden rounded-xl p-4 shadow-feature-card dark:shadow-feature-card-dark lg:p-6">
-        <div className="flex items-center gap-2">
-          <ZapIcon className="size-[18px]" />
-          <h2 className="text-sm font-light">Stacks</h2>
-        </div>
-        <Marquee gap="20px" className="py-4" fade pauseOnHover>
-          <SiHtml5 className="size-10" />
-          <SiCss3 className="size-10" />
-          <SiJavascript className="size-10" />
-          <SiTypescript className="size-10" />
-          <SiFigma className="size-10" />
-          <SiTailwindcss className="size-10" />
-          <SiNextdotjs className="size-10" />
-          <SiReact className="size-10" />
-          <SiPython className="size-10" />
-          <SiPostgresql className="size-10" />
-        </Marquee>
-        <Marquee gap="20px" className="py-4" reverse fade pauseOnHover>
-          <SiPrisma className="size-10" />
-          <SiMysql className="size-10" />
-          <SiFirebase className="size-10" />
-          <SiGit className="size-10" />
-          <SiVite className="size-10" />
-          <SiVisualstudiocode className="size-10" />
-          <SiCloudflare className="size-10" />
-          <SiMarkdown className="size-10" />
-          <SiJest className="size-10" />
-          <SiNodedotjs className="size-10" />
-        </Marquee>
+    <div className="container ">
+      <Heading heading="Technologies" />
+      <div className="flex gap-2 justify-center items-center mt-7 mb-10 flex-wrap container">
+        <SiHtml5 className="size-10" />
+        <SiCss3 className="size-10" />
+        <SiJavascript className="size-10" />
+        <SiTypescript className="size-10" />
+        <SiReact className="size-10" />
+        <SiRedux className="size-10" />
+        <SiFigma className="size-10" />
+        <SiTailwindcss className="size-10" />
+        <SiMaterialdesign className="size-10" />
+        <SiDaisyui className="size-10" />
+        <SiNextdotjs className="size-10" />
+        <SiNodedotjs className="size-10" />
+        <SiExpress className="size-10" />
+        <SiFastify className="size-10" />
+        <SiMysql className="size-10" />
+        <SiMongodb className="size-10" />
+        <SiFirebase className="size-10" />
+        <SiDocker className="size-10" />
+        <SiPostman className="size-10" />
+        <SiDbeaver className="size-10" />
+        <SiMoonrepo className="size-10" />
+        <SiTurborepo className="size-10" />
+        <SiVisualstudiocode className="size-10" />
+        <SiCplusplus className="size-10" />
+        <SiGit className="size-10" />
+        <SiGithub className="size-10" />
+        <SiSequelize className="size-10" />
+        <SiMongoose className="size-10" />
+        <SiTrpc className="size-10" />
+        <SiStripe className="size-10" />
+        <SiSocketdotio className="size-10" />
       </div>
     </div>
   );
